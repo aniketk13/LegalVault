9f61c168d30a38ae739310592267c382791b40b4
aniketk13 <ani.khajanchi257@gmail.com>
2023-09-16 16:45:53 +0530

    api and firebase configured

--- app/src/main/java/com/teamdefine/legalvault/MainActivity.kt
+++ app/src/main/java/com/teamdefine/legalvault/MainActivity.kt
@@ -2,8 +2,12 @@ package com.teamdefine.legalvault
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.teamdefine.legalvault.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
