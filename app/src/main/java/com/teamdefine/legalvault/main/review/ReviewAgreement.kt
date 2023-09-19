2e2f53e7ca7f268f0ca052d32cb2d0be4512ee5a
Nitish Sharma <nitish.sharma1186@gmail.com>
2023-09-20 00:31:19 +0530

    added review agreement screen designs

--- app/src/main/java/com/teamdefine/legalvault/main/review/ReviewAgreement.kt
+++ app/src/main/java/com/teamdefine/legalvault/main/review/ReviewAgreement.kt
@@ -1,25 +1,26 @@
 package com.teamdefine.legalvault.main.review
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.teamdefine.legalvault.databinding.FragmentReviewAgreementBinding
+import com.teamdefine.legalvault.main.utility.Utility
 
 class ReviewAgreement : Fragment() {
-
     private lateinit var viewModel: ReviewAgreementViewModel
     private lateinit var binding: FragmentReviewAgreementBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View?=FragmentReviewAgreementBinding.inflate(layoutInflater,container,false).also {
-        binding=it
+    ): View? = FragmentReviewAgreementBinding.inflate(layoutInflater, container, false).also {
+        binding = it
     }.root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.documentEditText.setText(Utility.getRandomText())
     }
 }
--- app/src/main/java/com/teamdefine/legalvault/main/utility/Utility.kt
+++ app/src/main/java/com/teamdefine/legalvault/main/utility/Utility.kt
@@ -29,4 +29,18 @@ object Utility {
             null
         }
     }
+
+    fun getRandomText(): String {
+        return "Certainly! Here's a randomly generated text of approximately 200 words:\n" +
+                "\n" +
+                "\"In the quiet stillness of the early morning, as the first rays of sunlight kissed the horizon, a sense of serenity enveloped the world. Birds began to chirp, breaking the silence with their cheerful melodies, while the gentle breeze whispered secrets to the swaying trees. It was a moment of pure, unadulterated beauty.\n" +
+                "\n" +
+                "In a bustling city, far removed from the tranquility of the countryside, people hurriedly made their way to work. Each person lost in their thoughts, chasing dreams and ambitions. The city was a symphony of honking horns, hurried footsteps, and the aroma of freshly brewed coffee wafting from street-side cafes.\n" +
+                "\n" +
+                "Meanwhile, a group of children played in a nearby park, their laughter filling the air with infectious joy. They raced each other, climbed on the playground equipment, and shared stories of imaginary adventures. Their innocence was a stark contrast to the complexities of adult life.\n" +
+                "\n" +
+                "As the day unfolded, the world continued its ceaseless motion. Some found love, while others faced heartbreak. Some achieved success, while others encountered setbacks. Life's unpredictability was its most defining characteristic.\n" +
+                "\n" +
+                "And so, the world turned, with each moment creating a tapestry of stories, emotions, and experiences. In the grand scheme of things, every individual was but a tiny speck in the vast cosmos, yet their lives were filled with meaning, purpose, and the possibility of endless discovery.\""
+    }
 }
